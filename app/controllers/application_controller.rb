4a0ff5cb1fe5c1fb4d68dc3ff359aa85d16186d9
chitrasrinivasan12 <csriniv@ncsu.edu>
2018-08-01 19:18:53 +0000

    it works!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,23 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+  
+  post '/results' do
+    answers = params.values 
+    @total = 0 
+    answers.each do |answer|
+      @total += answer.to_i 
+    end
+    puts @total 
+    
+    @combo = health_generator(@total)
+    if @combo == "OP1"
+      erb :OP1
+    elsif @combo == "OP2"
+      erb :OP2
+    elsif @combo == "OP3"
+      erb :OP3
+    end 
+  end
+  
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,11 @@
-class SampleModel
-  attr_accessor :name, :age
+def health_generator(user_value)
+  
+  if user_value == 27
+    return "OP1"
+  elsif user_value == 22
+    return "OP2"
+  elsif user_value == 15
+    return "OP3"
+  end
+  
 end
